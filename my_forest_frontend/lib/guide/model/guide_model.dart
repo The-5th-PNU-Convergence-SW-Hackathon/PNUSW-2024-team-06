c04b575a3d43d12aed88f07fb6b162b9fd7b1989
yeongwoo <jyy0223@naver.com>
2024-08-22 22:41:29 +0900

    [feat] guide model, provider 정의

---
+++ my_forest_frontend/lib/guide/model/guide_model.dart
@@ -0,0 +1,13 @@
+class GuideModel {
+  final String id;
+  final String title;
+  final String engTitle;
+  final String description;
+
+  GuideModel({
+    required this.id,
+    required this.title,
+    required this.engTitle,
+    required this.description,
+  });
+}
---
+++ my_forest_frontend/lib/guide/provider/guide_provider.dart
@@ -0,0 +1,35 @@
+import 'package:collection/collection.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_forest_frontend/common/const/data.dart';
+
+import '../model/guide_model.dart';
+
+final guideDetailProvider = Provider.family<GuideModel?, String>((ref, id) {
+  final events = ref.watch(guideProvider);
+
+  return events.where((element) => element.id == id).firstOrNull;
+});
+
+final guideProvider =
+    StateNotifierProvider<GuideStateNotifier, List<GuideModel>>(
+  (ref) => GuideStateNotifier(),
+);
+
+class GuideStateNotifier extends StateNotifier<List<GuideModel>> {
+  GuideStateNotifier() : super([]) {
+    initItems();
+  }
+
+  void initItems() {
+    state = guideTitles.keys
+        .mapIndexed(
+          (index, element) => GuideModel(
+            id: index.toString(),
+            title: element,
+            engTitle: guideTitles[element]!,
+            description: guideDescriptions[index],
+          ),
+        )
+        .toList();
+  }
+}
